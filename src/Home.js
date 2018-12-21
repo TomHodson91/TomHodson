4ea7f1ddb244101b32b85a6195b37bf57be6bf70
Tom Hodson <tomhodson91@hotmail.com>
2018-12-21 11:08:29 +0000

    Changed icons to svgs and formatting

--- src/Home.js
+++ src/Home.js
@@ -1,54 +1,58 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import github from './Images/GitHub-Mark/PNG/GitHub-Mark-120px-plus.png'
-import linkedIn from './Images/iconmonstr-linkedin-3-240.png'
-import email from './Images/iconmonstr-email-1-240.png'
+import github from './Images/iconmonstr-github-1.svg'
+import linkedIn from './Images/iconmonstr-linkedin-3.svg'
+import email from './Images/iconmonstr-email-1.svg'
 import phone from './Images/iconmonstr-phone-8.svg'
 
 export default function Home() {
     return (
         <div className="bg">
             <div className='banner'>
-            <Link to= {"/contact"}>
-                    <div className='icon'>
-                    <img src={github} alt="" />
-                    </div>
-            </Link>
-            <Link to= {"/contact"}>
-                    <div className='icon'>
-                    <img src={linkedIn} alt="" />
-                    </div>
-            </Link>
-            <Link to= {"/contact"}>
-                    <div className='icon'>
-                    <img src={email} alt="" />
-                    </div>
-            </Link>
-            <Link to= {"/contact"}>
-                    <div className='icon'>
-                    <img src={phone} alt="" />
-                    </div>
-            </Link>
-                <Link to= {"/"}>
-                    <div className='links'>
-                        Home
-                    </div>
-                </Link>
-                <Link to= {"/about"}>
-                    <div className='links'>
-                        About Me
-                    </div>
-                </Link>
-                <Link to= {"/projects"}>
-                    <div className='links'>
-                        Projects
-                    </div>
-                </Link>
-                <Link to= {"/contact"}>
-                    <div className='links'>
-                        Contact Me
-                    </div>
-                </Link>
+                <div className='banner1'>
+                    <a href='https://github.com/TomHodson91'>
+                            <div className='icon'>
+                            <img src={github} alt="" />
+                            </div>
+                    </a>
+                    <a href='https://www.linkedin.com/in/tom-hodson-684ab6169/'>
+                            <div className='icon'>
+                            <img src={linkedIn} alt="" />
+                            </div>
+                    </a>
+                    <a href='mailto:tomhodson91@hotmail.com'>
+                            <div className='icon'>
+                            <img src={email} alt="" />
+                            </div>
+                    </a>
+                    <Link to= {"/contact"}>
+                            <div className='icon'>
+                            <img src={phone} alt="" />
+                            </div>
+                    </Link>
+                </div>
+                <div className='banner2'>
+                    <Link to= {"/"}>
+                        <div className='links'>
+                            Home
+                        </div>
+                    </Link>
+                    <Link to= {"/about"}>
+                        <div className='links'>
+                            About Me
+                        </div>
+                    </Link>
+                    <Link to= {"/projects"}>
+                        <div className='links'>
+                            Projects
+                        </div>
+                    </Link>
+                    <Link to= {"/contact"}>
+                        <div className='links'>
+                            Contact Me
+                        </div>
+                    </Link>
+                </div>
             </div>
             <div className='title'>
                 Hi, I'm Tom - Junior JavaScript/Reactjs developer!
--- src/contact.js
+++ src/contact.js
@@ -10,6 +10,9 @@ export default function contact(){
                         Home
                     </div>
             </Link>
-            <a href={cv}>Recruiters HMU</a>
+            <h1><a href={cv}>Tom HODSON CV</a></h1>
+            <h1><a href='mailto:tomhodson91@hotmail.com'>Email: tomhodson91@hotmail.com</a></h1>
+            <h1><a href='https://www.linkedin.com/in/tom-hodson-684ab6169/'>LinkedIn: Junior JavaScript/React Developer</a></h1>
+            <h1>Phone: 07816390638</h1>
         </div>
     )}
